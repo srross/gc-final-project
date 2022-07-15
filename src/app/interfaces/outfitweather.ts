dfc5ed33957c26ef05986a0345fc78a7b0963517
Shawn Penning <penningsp@hotmail.com>
2022-07-15 16:51:38 -0400

    Added 3 interfaces

---
+++ src/app/interfaces/outfitweather.ts
@@ -0,0 +1,11 @@
+export interface Outfitweather {
+    Id: number;
+    MinTemperature: number;
+    OutfitBottom: string;
+    OutfitTop: string;
+    OutfitHead: string;
+    OutfitHands: string;
+    OutfitImage: string;
+    UserId: number;
+    MaxTemperature: number;
+}
