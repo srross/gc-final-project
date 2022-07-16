60828b21a40b38e3070f93ed455372c32da64996
Finesstro <jake_joe22@yahoo.com>
2022-07-16 12:39:33 -0400

    Added bootstrap, routing set ups, basic scaffolding.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { AdjustComponent } from './components/adjust/adjust.component';
+import { OutfitComponent } from './components/outfit/outfit.component';
+  
+  const routes: Routes = [
+    {path:'', redirectTo:'home', pathMatch:"full"},
+    {path:"home", component:HomeComponent},
+    {path:"outfit/:id",component:OutfitComponent},
+    {path:"adjust/:id",component:AdjustComponent}      
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
