3bfb0326988f5c8314e95894f3f5030b062dd814
Shawn Penning <penningsp@hotmail.com>
2022-07-18 22:03:17 -0400

    Logic to switch screens after login on Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AdjustComponent } from './components/adjust/adjust.component';
 import { OutfitComponent } from './components/outfit/outfit.component';
+import { AuthGuard } from '@auth0/auth0-angular';
   
   const routes: Routes = [
     {path:'', redirectTo:'home', pathMatch:"full"},
     {path:"home", component:HomeComponent},
+    {path:"home/:id", component:HomeComponent, canActivate: [AuthGuard]},
     {path:"outfit/:id",component:OutfitComponent},
-    //{path:"outfit",component:OutfitComponent},
     {path:"adjust/:id",component:AdjustComponent}      
 ];
 
